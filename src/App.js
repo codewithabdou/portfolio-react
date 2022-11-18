ad02acaad7fd9a977c3e9c737c5ff5b0b36e1145
Habouche khaled Abderrahmène <kk_habouche@esi.dz>
2022-11-18 18:55:20 +0100

    adding bgs

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
         </Routes>
-        <Footer />
+      <Footer />
       </main>
     </>
   );
